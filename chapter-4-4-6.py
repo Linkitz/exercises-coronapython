d256cba7e72f1699c3cdb2eabebfc85741bc6902
Linkitz <carloseduardopossamai@gmail.com>
2020-05-22 13:20:56 -0300

    trabalhos

--- chapter-4-4-6.py
+++ chapter-4-4-6.py
@@ -1,4 +1,5 @@
 # 4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.
-numbers = range(1,21,2)
-for i in numbers:
-    print(str(i))
+oddnumbers = list(range(1,21,2))
+
+for oddnumber in oddnumbers:
+    print(oddnumber)
